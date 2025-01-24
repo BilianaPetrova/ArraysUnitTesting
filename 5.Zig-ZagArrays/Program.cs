57282d2bd631b912e4e108fa0da431605b27d783
Bilyana <Bilyana@BIBI-LAPTOP>
2025-01-24 23:39:27 +0200

    Add project files.

---
+++ 5.Zig-ZagArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] firstArray = new int[n];
+int[] secondArray = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    int[] twoNumbers = Console.ReadLine()
+                       .Split(" ")
+                       .Select(int.Parse)
+                       .ToArray();
+    int firstNumber = twoNumbers[0];
+    int secondNumber = twoNumbers[1];
+
+    if (i % 2 == 0)
+    {
+        firstArray[i] = firstNumber;
+        secondArray[i] = secondNumber;
+
+    }
+    else
+    {
+        firstArray[i] = secondNumber;
+        secondArray[i] = firstNumber;
+    }
+
+}
+Console.WriteLine(string.Join(" ", firstArray));
+Console.WriteLine(string.Join(" ", secondArray));
